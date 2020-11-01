64871f4453fbd1d0065f95212a4ccbfdadd5dc76
roshid2500 <47004864+roshid2500@users.noreply.github.com>
2020-11-01 17:36:55 -0500

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,33 @@
+import pymongo
+import os
+import json 
+import csv 
+import sys, getopt, pprint
+import pandas as pd 
+
+
+def main():
+    #get connection
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    
+    #create a database called chess
+    db = myclient.chess
+    
+    #create a collection called posts in chess 
+    posts = db.posts
+    
+   
+    #open the csv file, NOTE CHANGE THE FILEPATH 
+    csvfile = open('../../../Users/roshi/Desktop/games.csv')
+    reader = csv.DictReader(csvfile)
+    header = ["id", "rated", "created_at", "last_move_at", "victory_status", "winner", 
+              "increment_code", "white_id", "white_rating", "black_id", "black_rating",
+              "moves", "opening_eco", "opening_name", "opening_ply"]
+    #insert in all the info into the database
+    for each in reader:
+        row = {}
+        for field in header:
+            row[field] = each[field]
+            posts.insert_one(row)  
+    csvfile.close()
+main()
